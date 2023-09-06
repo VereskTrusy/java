26b5668379542d92f612eaa40184f15417437a1f
VereskTrusy <elpt1987@gmail.com>
2023-09-07 00:37:23 +0900

    클래스, 상속, 다형성 공부한 노션보며 복습하고 추상클래스 다시 보는 중 의외로 다시보니 이해되는듯 재밋다ㅋㅋ 291page

---
+++ study/src/chapter02/ex06_AbstractClass/ex02_AbstractComputer/ComputerTest.java
@@ -0,0 +1,9 @@
+package chapter02.ex06_AbstractClass.ex02_AbstractComputer;
+
+public class ComputerTest {
+
+//    Computer pc1 = new Computer(); // 오류 발생 : 구현하지 않은 메서드를 구현하시오
+    Computer pc2 = new Desktop();
+//    Computer pc3 = new NoteBook(); // 오류 발생 :  구현하지 않은 메서드를 구현하시오
+    Computer pc4 = new MyNoteBook();
+}
