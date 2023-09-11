35981f723b8b0c46c291a076916334f876cb9300
veresk <elpt1987@gmail.com>
2023-09-12 08:39:13 +0900

    296 page. 플레이어 레벨 실습 했다 상속과 다형성 이해하기 조금 좋은듯

---
+++ study/src/chapter02/ex06_AbstractClass/ex05_GameLevel/PlayerLevel.java
@@ -0,0 +1,17 @@
+package chapter02.ex06_AbstractClass.ex05_GameLevel;
+
+public abstract class PlayerLevel {
+    public abstract void run();
+    public abstract void jump();
+    public abstract void turn();
+    public abstract void showLevelMessage();
+
+
+    final public void go(int count){
+        run();
+        for(int i = 0; i < count; i++){
+            jump();
+        }
+        turn();
+    }
+}
